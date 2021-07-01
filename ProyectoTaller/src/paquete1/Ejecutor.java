51dcd11723587b1ee1fa614648d0bc69435ce9e9
JamilErasmo <jamilerasmoq@gmail.com>
2021-07-01 16:38:12 -0500

    taller6

--- ProyectoTaller/src/paquete1/Ejecutor.java
+++ ProyectoTaller/src/paquete1/Ejecutor.java
@@ -13,26 +13,18 @@ public class Ejecutor {
 
     public static void main(String[] args) {
 
-        // nombre del archivo
-        String nombreArchivo = "pasajes.data";
+          String nombreArchivo = "pasajes.data";
         ArrayList<PasajeInterCantonal> pasajes = new ArrayList<>();
-
-        PasajeNormal p1 = new PasajeNormal("Luis Carrillo", "1122334455",
-                "Suces", 3, 1, 2);
-        PasajeNormal p2 = new PasajeNormal("Angel Mogrovejo", "7896855123",
-                "Algelia", 5, 1.5, 4);
+        PasajeMenorEdad pasajeM = new PasajeMenorEdad("Jamil", 1105595985, "Loja",
+        "loja", 40, 5, 10);
         
-        PasajeMenorEdad p3 = new PasajeMenorEdad();
+        pasajes.add(pasajeM);
+        
+        EscrituraArchivoSecuencial archivo = new EscrituraArchivoSecuencial(nombreArchivo);
 
-        pasajes.add(p1);
-        pasajes.add(p2);
-        pasajes.add(p3);
-
-        EscrituraArchivoSecuencial archivo = new EscrituraArchivoSecuencial(
-                nombreArchivo);
 
         for (int i = 0; i < pasajes.size(); i++) {
-
+            
             // establecer el valor del atributo registro
             archivo.establecerRegistro(pasajes.get(i));
             // establecer en el archivo el atributo del registro
@@ -41,8 +33,7 @@ public class Ejecutor {
 
         archivo.cerrarArchivo();
 
-        LecturaArchivoSecuencial lectura = new LecturaArchivoSecuencial(
-                nombreArchivo);
+        LecturaArchivoSecuencial lectura = new LecturaArchivoSecuencial(nombreArchivo);
         lectura.establecerListaPasajes();
         System.out.println(lectura);
         lectura.cerrarArchivo();
